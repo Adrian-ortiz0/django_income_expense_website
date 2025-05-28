8afe7c060b84ba0e61604b2ac08d8513fc291dcf
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-05-28 10:12:57 -0500

    feat: :sparkles: We already can send the verification email

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UserNameValidationView, EmailValidationView
+from .views import RegistrationView, UserNameValidationView, EmailValidationView, VerificationView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('validate-username', csrf_exempt(UserNameValidationView.as_view()),
          name="validate-username"),
-    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate_email')
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate_email'),
+    path('activate/<uidb64>/<token>', VerificationView.as_view(),name="activate")
 ]
--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,10 +1,17 @@
 import json
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views import View
 from django.contrib.auth.models import User
 from validate_email import validate_email
 from django.contrib import messages
+from django.core.mail import EmailMessage
+from .utils import token_generator
+
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
 # Create your views here.
 
 class RegistrationView(View):
@@ -29,7 +36,30 @@ class RegistrationView(View):
                     return render(request, 'authentication/register.html', context)
                 user = User.objects.create_user(username=username, email=email)
                 user.set_password(password)
+                user.is_active = False
                 user.save()
+                email_subject = 'Activate your account'
+                # path_to_view 
+                # - getting domain we are on
+                # - relative url to verification
+                # - encode uid 
+                # - token
+                
+                uidb64= urlsafe_base64_encode(force_bytes(user.pk))
+                
+                
+                domain=get_current_site(request).domain
+                link=reverse('activate', kwargs={'uidb64': uidb64,'token': token_generator.make_token(user)})
+                activate_url = f"http://{domain}{link}"
+                
+                email_body = f"Hi {user.username} please use this link to verify your account\n {activate_url}"
+                email = EmailMessage(
+                    email_subject,
+                    email_body,
+                    "noreply@semycolo.com",
+                    [email],
+                )
+                email.send(fail_silently=False)
                 messages.success(request, 'Account created successfully')
         return render(request, 'authentication/register.html')
         
@@ -59,3 +89,8 @@ class UserNameValidationView(View):
         
         
         return JsonResponse({'username_valid': True})
+    
+class VerificationView(View):
+    def get(self, request, uidb64, token):
+        return redirect('login')
+    
