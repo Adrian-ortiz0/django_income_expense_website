a0ee78065c802b5043861c93e18a43b10a992be6
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-06-03 11:40:58 -0500

    feat: :sparkles: logout process was finished

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UserNameValidationView, EmailValidationView, VerificationView, LoginView
+from .views import RegistrationView, UserNameValidationView, EmailValidationView, VerificationView, LoginView, LogoutView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('validate-username', csrf_exempt(UserNameValidationView.as_view()),
          name="validate-username"),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate_email'),
--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -11,7 +11,9 @@ from .utils import token_generator
 
 from django.utils.encoding import force_bytes, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_str
 from django.contrib.sites.shortcuts import get_current_site
+from django.contrib import auth
 # Create your views here.
 
 class RegistrationView(View):
@@ -94,7 +96,7 @@ class UserNameValidationView(View):
 class VerificationView(View):
     def get(self, request, uidb64, token):
         try:
-            id=force_text(urlsafe_base64_decode(uidb64))
+            id=force_str(urlsafe_base64_decode(uidb64))
             user=User.objects.get(pk=id)
             
             if not token_generator.check_token(user, token):
@@ -115,3 +117,27 @@ class LoginView(View):
     def get(self, request):
         return render(request, 'authentication/login.html')
     
+    def post(self, request):
+        username=request.POST['username']
+        password = request.POST['password']
+        
+        if username and password:
+            user=auth.authenticate(username=username, password=password)
+            
+            if user:
+                if user.is_active:
+                    auth.login(request, user)
+                    messages.success(request, 'Welcome, ' + user.username+ ' You are now logged in')
+                    return redirect('expenses')
+                messages.error(request, 'Account is not active, please check your email')
+                return render(request, 'authentication/login.html')
+            messages.error(request, 'Invalid credentials, try again')
+            return render(request, 'authentication/login.html')
+        messages.error(request, 'Please fill all fields')
+        return render(request, 'authentication/login.html')
+    
+class LogoutView(View):
+    def post(self, request):
+        auth.logout(request)
+        messages.success(request, 'You have been logged out successfully')
+        return redirect('login')
